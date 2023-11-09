ecedf84d14876ed032f7f57db5e446928a8fa160
iceprins <junghankyul@gmail.com>
2023-11-09 20:55:55 +0900

    feat: 상수 클래스 생성

---
+++ src/main/java/christmas/service/Constant.java
@@ -0,0 +1,8 @@
+package christmas.service;
+
+public class Constant {
+    public static final String ERROR_MESSAGE = "[ERROR] ";
+    public static final String NOTHING_DATE = "날짜를 입력하지 않았습니다. 다시 입력해 주세요.";
+    public static final String NOT_DIGIT = "숫자를 입력해야 합니다. 다시 입력해 주세요.";
+    public static final String INVALID_DATE = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+}
