eb536b7acab54cdc7267d675887885b3c9aad6d3
iceprins <junghankyul@gmail.com>
2023-11-10 18:57:51 +0900

    feat: isInMenu 메소드 추가

--- src/main/java/christmas/service/OrderedMenuValidator.java
+++ src/main/java/christmas/service/OrderedMenuValidator.java
@@ -1,5 +1,6 @@
 package christmas.service;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -14,6 +15,15 @@ public class OrderedMenuValidator {
         String std = "(^[가-힣]*)-[0-9]{1,2}";
         for (String s : input) {
             if (!Pattern.matches(std, s)) {
+                throw new IllegalArgumentException(Constant.ERROR_MESSAGE + Constant.INVALID_MENU);
+            }
+        }
+    }
+
+    private static void isInMenu(List<String> menus, List<String> input) {
+        for (String s : input) {
+            List<String> temp = Arrays.asList(s.split("-"));
+            if (!menus.contains(temp.get(0))) {
                 throw new IllegalArgumentException(Constant.ERROR_MESSAGE + Constant.INVALID_MENU);
             }
         }
