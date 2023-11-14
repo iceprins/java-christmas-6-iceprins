539685e4ad23dfe40e16682f1f84b0332d0685f0
iceprins <junghankyul@gmail.com>
2023-11-14 13:44:58 +0900

    refactor: readOrder() 반환 타입 수정 및 출력할 문자열 Constant 클래스로 이전

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,19 +1,19 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.service.Constant;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class InputView {
-
-    static final String READ_VISIT_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
-    static final String READ_ORDER = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
-
     public String readVisitDate() {
-        System.out.println(READ_VISIT_DATE);
+        System.out.println(Constant.READ_VISIT_DATE);
         return Console.readLine();
     }
 
-    public String readOrder() {
-        System.out.println(READ_ORDER);
-        return Console.readLine();
+    public List<String> readOrder() {
+        System.out.println(Constant.READ_ORDER);
+        return Arrays.asList(Console.readLine().split(","));
     }
 }
