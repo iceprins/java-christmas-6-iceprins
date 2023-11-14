7c46f275bcbfb3015cc1be2f18ce95ed1dd8281a
iceprins <junghankyul@gmail.com>
2023-11-14 13:47:38 +0900

    refactor: 새로운 문자열 상수 추가

--- src/main/java/christmas/service/Constant.java
+++ src/main/java/christmas/service/Constant.java
@@ -1,8 +1,9 @@
 package christmas.service;
 
 public class Constant {
+    public static final String READ_VISIT_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    public static final String READ_ORDER = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
     public static final String ERROR_MESSAGE = "[ERROR] ";
-    public static final String NOTHING_DATE = "날짜를 입력하지 않았습니다. 다시 입력해 주세요.";
-    public static final String NOT_DIGIT = "숫자를 입력해야 합니다. 다시 입력해 주세요.";
     public static final String INVALID_DATE = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    public static final String INVALID_MENU = "유효하지 않은 주문입니다. 다시 입력해 주세요.";
 }
