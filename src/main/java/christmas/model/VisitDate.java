be905816e3a7de6adf7a2a4067ceb82294b0d906
iceprins <junghankyul@gmail.com>
2023-11-15 22:24:15 +0900

    feat: getBadge 메소드 추가

--- src/main/java/christmas/model/VisitDate.java
+++ src/main/java/christmas/model/VisitDate.java
@@ -9,6 +9,9 @@ public class VisitDate {
     static final int PRESENT_EVENT_CRITERION = 120000;
     static final int SPECIAL_DISCOUNT_AMOUNT = 1000;
     static final int PRESENT_EVENT_AMOUNT = 25000;
+    static final int STAR_BASE_AMOUNT = 5000;
+    static final int TREE_BASE_AMOUNT = 10000;
+    static final int SANTA_BASE_AMOUNT = 20000;
     static final int EVENT_BASE_AMOUNT = 10000;
 
     private final int date;
@@ -28,6 +31,19 @@ public class VisitDate {
             calculatedSpecialDiscount();
             calculatedPresentEvent(totalBeforeDc);
         }
+    }
+
+    public String getBadge(int totalBenefit) {
+        if (totalBenefit >= STAR_BASE_AMOUNT && totalBenefit < TREE_BASE_AMOUNT) {
+            return OutputViewConstant.EVENT_BADGE_STAR;
+        }
+        if (totalBenefit >= TREE_BASE_AMOUNT && totalBenefit < SANTA_BASE_AMOUNT) {
+            return OutputViewConstant.EVENT_BADGE_TREE;
+        }
+        if (totalBenefit >= SANTA_BASE_AMOUNT) {
+            return OutputViewConstant.EVENT_BADGE_SANTA;
+        }
+        return null;
     }
 
     private void calculateChristmasDiscount() {
