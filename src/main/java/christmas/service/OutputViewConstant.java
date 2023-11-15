d3f5b78b68303b46a144c42431682fabb0e12755
iceprins <junghankyul@gmail.com>
2023-11-15 22:05:27 +0900

    feat: OutputViewConstant 클래스 생성

---
+++ src/main/java/christmas/service/OutputViewConstant.java
@@ -0,0 +1,22 @@
+package christmas.service;
+
+public class OutputViewConstant {
+    public static final String WELCOME_MSG = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    public static final String PREVIEW_MSG = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
+    public static final String ORDERED_MENU_TITLE = "<주문 메뉴>";
+    public static final String TOTAL_BEFORE_DC_TITLE = "<할인 전 총주문 금액>";
+    public static final String PRESENT_MENU_TITLE = "<증정 메뉴>";
+    public static final String BENEFIT_HISTORY_TITLE = "<혜택 내역>";
+    public static final String TOTAL_BENEFIT_TITLE = "<총혜택 금액>";
+    public static final String TOTAL_AFTER_DC_TITLE = "<할인 후 예상 결제 금액>";
+    public static final String EVENT_BADGE_TITLE = "<12월 이벤트 배지>";
+    public static final String MENU_UNIT = "%s %d개";
+    public static final String MONEY_UNIT = "%s원\n";
+    public static final String CHAMPAGNE = "샴페인 1개";
+    public static final String NOTHING = "없음";
+    public static final String COLON = ": ";
+    public static final String WON = "원";
+    public static final String EVENT_BADGE_STAR = "별";
+    public static final String EVENT_BADGE_TREE = "트리";
+    public static final String EVENT_BADGE_SANTA = "산타";
+}
