333457ed14e32cab4451fad5f3b218bf69d69b84
iceprins <junghankyul@gmail.com>
2023-11-15 22:27:40 +0900

    feat: printMenu 메소드 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -2,6 +2,9 @@ package christmas.view;
 
 import christmas.service.OutputViewConstant;
 
+import java.util.List;
+import java.util.Map;
+
 public class OutputView {
     public void printWelcome() {
         System.out.println(OutputViewConstant.WELCOME_MSG);
@@ -9,5 +12,13 @@ public class OutputView {
 
     public void printPreviewMessage(int date) {
         System.out.printf(OutputViewConstant.PREVIEW_MSG + System.lineSeparator(), date);
+    }
+
+    public void printMenu(Map<String, Integer> orderPair) {
+        System.out.println(OutputViewConstant.ORDERED_MENU_TITLE);
+        for (Map.Entry<String, Integer> pair : orderPair.entrySet()) {
+            System.out.printf(OutputViewConstant.MENU_UNIT + System.lineSeparator(), pair.getKey(), pair.getValue());
+        }
+        System.out.print(System.lineSeparator());
     }
 }
