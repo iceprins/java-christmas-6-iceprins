369ff64d61e8d2cfbae0c3baa55144fd3d923a96
iceprins <junghankyul@gmail.com>
2023-11-15 21:52:44 +0900

    feat: calculateWeekdayDiscount 메소드 추가

--- src/main/java/christmas/model/VisitDate.java
+++ src/main/java/christmas/model/VisitDate.java
@@ -1,6 +1,10 @@
 package christmas.model;
 
+import java.util.*;
+
 public class VisitDate {
+    static final int WEEK_DISCOUNT = 2023;
+
     private final int date;
     public VisitDate(int date) {
         this.date = date;
@@ -16,5 +20,11 @@ public class VisitDate {
             discount = 1000 + 100 * (date - 1);
         }
         Discount.CHRISTMAS.setAmount(discount);
+    }
+
+    private void calculateWeekdayDiscount(Map<String, Integer> orders) {
+        if (date % 7 != 1 && date % 7 != 2) {
+            Discount.WEEKDAY.setAmount(Order.getNumberOfDessert(orders) * WEEK_DISCOUNT);
+        }
     }
 }
