e6363fd75c8477ae9fe54c173292e03f6d63a772
iceprins <junghankyul@gmail.com>
2023-11-15 22:25:34 +0900

    feat: printWelcome 메소드 추가

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,9 @@
+package christmas.view;
+
+import christmas.service.OutputViewConstant;
+
+public class OutputView {
+    public void printWelcome() {
+        System.out.println(OutputViewConstant.WELCOME_MSG);
+    }
+}
