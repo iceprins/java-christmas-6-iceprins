55befaae01492134c442c6091fcf7d30129b3851
iceprins <junghankyul@gmail.com>
2023-11-15 21:45:21 +0900

    feat: VisitDate 클래스 생성

---
+++ src/main/java/christmas/model/VisitDate.java
@@ -0,0 +1,12 @@
+package christmas.model;
+
+public class VisitDate {
+    private final int date;
+    public VisitDate(int date) {
+        this.date = date;
+    }
+
+    public int getDate() {
+        return date;
+    }
+}
