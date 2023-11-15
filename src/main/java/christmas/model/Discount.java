79dccbf176087aa921babb00e583c74cf64ae397
iceprins <junghankyul@gmail.com>
2023-11-15 22:06:50 +0900

    feat: Discount 클래스 생성

---
+++ src/main/java/christmas/model/Discount.java
@@ -0,0 +1,29 @@
+package christmas.model;
+
+public enum Discount {
+    CHRISTMAS("크리스마스 디데이 할인", 0),
+    WEEKDAY("평일 할인", 0),
+    WEEKEND("주말 할인", 0),
+    SPECIAL("특별 할인", 0),
+    PRESENT_EVENT("증정 이벤트", 0);
+
+    private final String discountType;
+    private int amount;
+
+    Discount(String discountType, int amount) {
+        this.discountType = discountType;
+        this.amount = amount;
+    }
+
+    public String getDiscountType() {
+        return discountType;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
