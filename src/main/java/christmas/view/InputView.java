30fcc961f2725fb747c5c64e4bc854702a3fa824
iceprins <junghankyul@gmail.com>
2023-11-15 21:34:33 +0900

    style: 수정된 Constant 클래스 새로운 이름 적용

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,19 +1,19 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import christmas.service.Constant;
+import christmas.service.InputViewConstant;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class InputView {
     public String readVisitDate() {
-        System.out.println(Constant.READ_VISIT_DATE);
+        System.out.println(InputViewConstant.READ_VISIT_DATE);
         return Console.readLine();
     }
 
     public List<String> readOrder() {
-        System.out.println(Constant.READ_ORDER);
+        System.out.println(InputViewConstant.READ_ORDER);
         return Arrays.asList(Console.readLine().split(","));
     }
 }
