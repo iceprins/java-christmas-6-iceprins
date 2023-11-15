941dcf35c183cf90f7c55718bf4153ac10d82984
iceprins <junghankyul@gmail.com>
2023-11-15 22:27:09 +0900

    feat: printPreviewMessage 메소드 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -6,4 +6,8 @@ public class OutputView {
     public void printWelcome() {
         System.out.println(OutputViewConstant.WELCOME_MSG);
     }
+
+    public void printPreviewMessage(int date) {
+        System.out.printf(OutputViewConstant.PREVIEW_MSG + System.lineSeparator(), date);
+    }
 }
